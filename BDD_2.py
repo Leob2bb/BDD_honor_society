c9b1064ec763df7ccf20893bd14c15bb2483a6e0
Leob2bb <leo437782@gmail.com>
2024-08-23 02:55:58 +0900

    명예학회 2번 문제 코드 수정, 기본적인 네트워크 jpeg 파일 생성

--- BDD_2.py
+++ BDD_2.py
@@ -4,26 +4,35 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 # 1. Carry.txt BDD 그리기
-s = ['x3', 'x2', 'x1', 'x0', 'y3', 'y2', 'y1', 'y0', '0', '1']
+s = ['x3', 'x2', 'x1', 'x0', 'y3', 'y2', 'y1', 'y0']
 # 읽어와도 될 것 같은데 -> 그러면 또 txt 파일 바꿔야 됨.
 
 tt1 = open('Carry.txt','r')
 g = nx.Graph()
 
-# node 그리기
+# node 그리기: x3 ~ y_0까지 8개를 트리 모양으로 쭉 펼친다.
 for i in range(len(s)):
-    g.add_node(s[i])
+    for _ in range(2**i):
+        g.add_node(s[i])
+g.add_node('0')
+g.add_node('1')
 
 # edge 그리기
-for i in range(len(s)-2):
+for i in range(len(s)):
     for j in range(i+1, len(s)):
         g.add_edge(s[i], s[j])
+g.add_edge('y0', '0')
+g.add_edge('y0', '1')
 
 
-# 좌표 설정
+# 좌표 설정: 트리 모양
+import random
+
 pos = {}
-for i in range(len(s)):
-    pos[s[i]] = (i%3/3, i//3/3)
+for node in s:
+    pos[node] = (random.random(), random.random())
+pos['0'] = (0, -1)
+pos['1'] = (1, -1)
 
 nx.draw(g, pos, with_labels = True)
 plt.show()
