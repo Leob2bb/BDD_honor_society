bae60918a8547e45fc208a5e5ea2318ec23ba102
Leob2bb <leo437782@gmail.com>
2024-08-23 03:39:49 +0900

    BDD_2_1.py라는 새로운 파일 생성, 기본 트리 구조 네트워크 시각화

--- BDD_2.py
+++ BDD_2.py
@@ -5,6 +5,8 @@ import networkx as nx
 
 # 1. Carry.txt BDD 그리기
 s = ['x3', 'x2', 'x1', 'x0', 'y3', 'y2', 'y1', 'y0']
+# s = [['x3'], ['x2'], ['x1'], ['x0'], ['y3'], ['y2'], ['y1'], ['y0']]
+
 # 읽어와도 될 것 같은데 -> 그러면 또 txt 파일 바꿔야 됨.
 
 tt1 = open('Carry.txt','r')
@@ -12,14 +14,14 @@ g = nx.Graph()
 
 # node 그리기: x3 ~ y_0까지 8개를 트리 모양으로 쭉 펼친다.
 for i in range(len(s)):
-    for _ in range(2**i):
-        g.add_node(s[i])
+    for num in range(2**i):
+        g.add_node(s[i] + f'_{num}')
 g.add_node('0')
 g.add_node('1')
 
 # edge 그리기
 for i in range(len(s)):
-    for j in range(i+1, len(s)):
+    for j in range(i + 1, len(s)):
         g.add_edge(s[i], s[j])
 g.add_edge('y0', '0')
 g.add_edge('y0', '1')
@@ -29,8 +31,8 @@ g.add_edge('y0', '1')
 import random
 
 pos = {}
-for node in s:
-    pos[node] = (random.random(), random.random())
+for i in s:
+    pos[s[i]] = (random.random(), 1-i/4)
 pos['0'] = (0, -1)
 pos['1'] = (1, -1)
 
---
+++ BDD_2_1.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# 1. 가장 기본 형태 BDD 그리기
+
+# https://frhyme.github.io/python-lib/nx_layout_for_tree/ 참조
+
+# 각 노드는 2개의 자식을 가지고, 트리의 높이는 9 (변수 9개)
+g = nx.generators.classic.balanced_tree(2, 9)
+pos = nx.spring_layout(g)
+
+# 트리 그래프를 시각화: 이거 트리 형태로는 안되나?
+plt.figure(figsize=(12, 12))
+nx.draw(g, pos, with_labels=True, node_size=50, node_color="lightblue", font_size=8, edge_color="gray")
+plt.show()
+
+
