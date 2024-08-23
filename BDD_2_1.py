9d4a1bac3a83edbc2a0f60151ef789285e528ab6
Leob2bb <leo437782@gmail.com>
2024-08-23 17:16:58 +0900

    2번 문제 망한 것 같아서 새로운 파일 생성함, dd나 Graphviz 라이브러리를 활용할 계획

--- BDD_2_1.py
+++ BDD_2_1.py
@@ -16,16 +16,24 @@ tt1.close()
 
 # node 그리기
 # https://frhyme.github.io/python-lib/nx_layout_for_tree/ 참조
-# 각 노드는 2개의 자식을 가지고, 트리의 높이는 9 (변수 9개)
-g = nx.generators.classic.balanced_tree(2, 9)
-pos = nx.spring_layout(g)
+# 각 노드는 2개의 자식을 가지고, 트리의 높이는 7
+g = nx.generators.classic.balanced_tree(2, 8) # 수정
+
+g.add_node('num0')
+g.add_node('num1')
 
 # edge, 가중치 추가하기: 노드의 홀짝성 같으면 가중치 1, 아니면 0
+
+for u in g.nodes(): # 수정필요
+    g.add_edge(str(u), 'num0')
+for u in range(511):
+    g.add_edge(str(u), 'num1')
+
 for u, v in g.edges():
-    if (u + v)%2 == 0:
-        g[u][v]['weight'] = 1
-    else:
-        g[u][v]['weight'] = 0
+    # if (u + v)%2 == 0:
+    g[u][v]['weight'] = 1
+    # else:
+    #     g[u][v]['weight'] = 0
 
 # 가중치에 따라 색깔 지정하기
 edge_colors = []
@@ -36,10 +44,18 @@ for u, v, data in g.edges(data = True):
     else:
         edge_colors.append('red')
 
-# 트리 그래프를 시각화: 이거 트리 형태로는 안되나?  
+# 트리 그래프를 시각화:
+pos = nx.spring_layout(g)
+
 plt.figure(figsize=(12, 12))
 nx.draw(g, pos, with_labels = True, edge_color = edge_colors, node_size = 50, node_color = "lightblue", font_size = 5)
 plt.show()
 
-# 각 edge에 가중치를 가해보자
-
+# 트리 끝부분에 0 또는 1 표시
+# i = 0
+# for node, (x, y) in pos.items():
+#     if node >= 256:
+#         plt.text(x, y, s = tt1_list[i][8]) # s_out
+#         if EOFError:
+#             break
+#         i += 1
